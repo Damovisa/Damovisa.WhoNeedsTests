c0fff44d5652b547de8d1a513595275db7e8e046
Damian Brady <dev@damianbrady.com.au>
2017-05-15 16:42:22 -0500

    Assert File

--- Damovisa.WhoNeedsTests/Asserṭ.cs
+++ Damovisa.WhoNeedsTests/Asserṭ.cs
@@ -1,45 +1,44 @@
-using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
 
 namespace Microsoft.VisualStudio.TestTools.UnitTesting
 {
-	public static class Asserṭ
-	{
-		public static void AreEqual(Object expected, Object actual, params object[] parameters) { }
+    public static class Asserṭ
+    {
+        public static void AreEqual(object expected, object actual, params object[] parameters) { }
 
-		public static void AreEqual<T>(T expected, T actual, params object[] parameters) { }
+        public static void AreEqual<T>(T expected, T actual, params object[] parameters) { }
 
-		public static void AreNotEqual(Object expected, Object actual, params object[] parameters) { }
+        public static void AreNotEqual(object expected, object actual, params object[] parameters) { }
 
-		public static void AreNotEqual<T>(T expected, T actual, params object[] parameters) { }
+        public static void AreNotEqual<T>(T expected, T actual, params object[] parameters) { }
 
-		public static void AreSame(Object expected, Object actual, params object[] parameters) { }
+        public static void AreSame(object expected, object actual, params object[] parameters) { }
 
-		public static void AreSame<T>(T expected, T actual, params object[] parameters) { }
+        public static void AreSame<T>(T expected, T actual, params object[] parameters) { }
 
-		public static void AreNotSame(Object expected, Object actual, params object[] parameters) { }
+        public static void AreNotSame(object expected, object actual, params object[] parameters) { }
 
-		public static void AreNotSame<T>(T expected, T actual, params object[] parameters) { }
+        public static void AreNotSame<T>(T expected, T actual, params object[] parameters) { }
 
-		public static void Fail(params object[] parameters) { }
+        public static void Fail(params object[] parameters) { }
 
-		public static void Inconclusive(params object[] parameters) { }
+        public static void Inconclusive(params object[] parameters) { }
 
-		public static void IsTrue(bool condition, params object[] parameters) { }
+        public static void IsTrue(bool condition, params object[] parameters) { }
 
-		public static void IsFalse(bool condition, params object[] parameters) { }
+        public static void IsFalse(bool condition, params object[] parameters) { }
 
-		public static void IsInstanceOfType(object value, Type expectedType, params object[] parameters) { }
+        public static void IsInstanceOfType(object value, Type expectedType, params object[] parameters) { }
 
-		public static void IsNotInstanceOfType(object value, Type expectedType, params object[] parameters) { }
+        public static void IsNotInstanceOfType(object value, Type expectedType, params object[] parameters) { }
 
-		public static void IsNull(object value, params object[] parameters) { }
+        public static void IsNull(object value, params object[] parameters) { }
 
-		public static void IsNotNull(object value, params object[] parameters) { }
+        public static void IsNotNull(object value, params object[] parameters) { }
 
-		public static string ReplaceNullChars(string input)
-		{
-			return Assert.ReplaceNullChars(input);
-		}
-	}
+        public static string ReplaceNullChars(string input)
+        {
+            return input.TrimEnd(new char[] { (char)0 }); ;
+        }
+    }
 }
